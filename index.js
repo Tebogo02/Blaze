f619098fee55f82c67fcaf43a7e35f43880a8ec1
Tebogo <142109324+Tebogo02@users.noreply.github.com>
2024-07-08 09:09:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// var iconThemeDark = document.getElementById("icon-1");
-// var iconThemeLight = document.getElementById("icon-2");
+var iconThemeDark = document.getElementById("icon-1");
+var iconThemeLight = document.getElementById("icon-2");
 
-// iconThemeDark.onclick = function(){
-//     document.body.style.backgroundColor = "Black";
-//     iconTheme.style.color = "white";
-// }
-// iconThemeLight.onclick = function(){
-//     document.body.style.backgroundColor = "White";
-//     iconTheme.style.color = "black";
-// }
-
-let numbers = [2, 34, 2, 4, 12, 67, 45];
-
-function double(element, index, array){
-    array[index] = element;
+iconThemeDark.onclick = function(){
+     document.body.style.backgroundColor = "Black";
+     iconTheme.style.color = "white";
+}
+ iconThemeLight.onclick = function(){
+     document.body.style.backgroundColor = "White";
+     iconTheme.style.color = "black";
 }
 
-numbers.forEach(double);
 
-console.log(numbers);
 
 
